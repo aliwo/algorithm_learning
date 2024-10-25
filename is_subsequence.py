8502b3ffa2ff0698ebf1f94ddbfa983e871084a0
SW <aliwo@naver.com>
2024-10-25 15:08:55 +0900

    is_subsequence 해결~

--- is_subsequence.py
+++ is_subsequence.py
@@ -1,1 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        s_i = 0
+        while i < len(t):
+            if s_i == len(s):
+                return True
+            if s[s_i] == t[i]:
+                s_i += 1
+            i += 1
+        return s_i == len(s)
 
+print(Solution().isSubsequence("abc", "ahbgdc"))
