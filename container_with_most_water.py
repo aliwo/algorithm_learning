9ce6dea098623725e73e791b24b7aa7fab99930a
SW <aliwo@naver.com>
2024-10-27 19:15:48 +0900

    solve container_with_most_water

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,21 +1,47 @@
+import heapq
+
+
 class Solution:
     def maxArea(self, height: list[int]) -> int:
         """
         https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=leetcode-75
 
-        아직 못 풀었다... 계속 시간 초과 남 ^^ 이거 풀었던 문제 같은데
+        와... 자력으로 풀긴 했는데 beats 5% 다.. 흑흑
         """
-        i = 0
-        j = len(height) - 1
-        result = 0
+        heap = []
+        far_left = (len(height))
+        far_right = 0
+        max_result = 0
+        for i, h in enumerate(height):
+            heapq.heappush(heap, (-h, i))
 
-        for i in range(0, len(height)):
-            max_right_height = 0
-            for j in reversed(range(i + 1, len(height))):
-                if height[j] > max_right_height:
-                    cur_water = (j - i) * min(height[j], height[i])
-                    if cur_water > result:
-                        result = cur_water
-                        max_right_height = height[j]
+        while heap:
+            elem, i = heapq.heappop(heap)
+            if i < far_left:
+                far_left = i
+            if i > far_right:
+                far_right = i
+            max_result = max(max_result, -elem * max(i - far_left, far_right - i))
 
-        return result
+        return max_result
+
+
+
+
+
+        # i = 0
+        # j = len(height) - 1
+        # result = 0
+        #
+        # for i in range(0, len(height)):
+        #     max_right_height = 0
+        #     for j in reversed(range(i + 1, len(height))):
+        #         if height[j] > max_right_height:
+        #             cur_water = (j - i) * min(height[j], height[i])
+        #             if cur_water > result:
+        #                 result = cur_water
+        #                 max_right_height = height[j]
+        #
+        # return result
+
+print(Solution().maxArea([2,3,10,5,7,8,9]))
